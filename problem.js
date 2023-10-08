2d0c1ad2c33caa0cf299a0eb1f779c7e15ed92d5
Ifthakarprogram <Ifthakarislamfahad29@gmail.com>
2023-10-08 21:25:50 +0600

    simple problem solved

---
+++ problem.js
@@ -0,0 +1,39 @@
+//  problem solved-1
+
+var harryMome = 1000;
+var apple = 400;
+var orange = 300;
+var totalCost = apple + orange;
+console.log(totalCost);
+
+var Apple_Orange = 700;
+var returnMom = harryMome - Apple_Orange;
+console.log(returnMom);
+
+// problem solved-2
+
+var math = 75.25;
+var biology = 65;
+var physice = 80;
+var chymistry = 35.45;
+var english = 99.50;
+
+var totalSum = math + biology + physice + chymistry + english;
+console.log(totalSum);
+
+var orginalsum = totalSum / 5;
+orginalsum = orginalsum.toFixed(2);
+console.log(orginalsum);
+
+// problem solved-3
+var line1 = "I am going to be";
+var line2 = "an awesome web developer";
+var line_meaning = line1 + " " + line2;
+console.log(line_meaning);
+
+// problem solved-4
+
+var inputNumber = 119;
+var saraAsked = 5;
+var sum = inputNumber % saraAsked;
+console.log(sum);
